8aa39123fd89520890ae01116a42f7fa7c6339a4
wmlim014 <166711243+wmlim014@users.noreply.github.com>
2024-11-11 12:48:58 +0800

    Update Program and README.md
    1. Update on read file and store data function.
    2. Write some instruction to README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# SystemSecurity_A3
+# SystemSecurity_A3 Compile in Window
+```
+python main.py
+```
+
+>[!NOTE]
+>1. Please open file in sub_program to modified the program for testing
+>2. Please declare a function for each functionality, this is easy to include the new code to the `main.py`
+>3. Before merging the program into `main.py` and push to origin, please inform team.
---
+++ sub_program/read_files.py
@@ -0,0 +1,98 @@
+# Declare variable needed
+events = []
+stats = []
+num_of_events = 0
+num_of_stats = 0
+
+# Read event file
+def read_event_file(input_string):
+    file_found = False
+    global num_of_events
+    file_name = input(input_string)
+
+    while not file_found:
+        try:
+            with open(file_name, "r") as file:
+                file_found = True # File found
+
+                num_of_events = int(file.readline().strip()) # Initialize file number of events with first line
+                
+                # Read file content and store it
+                for line in file:
+                    data = (line.split(':'))
+
+                    # Declare distionary for each event
+                    event_dist = {
+                        "event_name": data[0].strip(),
+                        "event_type": data[1].strip()
+                    }
+
+                    # If the event is countinuous event
+                    if(event_dist["event_type"] == 'C'):
+                        event_dist["min"] = round(float(data[2].strip()), 2)
+                        event_dist["max"] = round(float(data[3].strip()), 2)
+                    
+                    else: # If the event is discrete events
+                        event_dist["min"] = int(data[2].strip())
+                        event_dist["max"] = int(data[3].strip())
+
+                    event_dist["alert"] = int(data[4].strip())
+                    
+                    events.append(event_dist)
+                    # print(event_dist)  # Debug line
+
+        except FileNotFoundError:
+            print("File not found. Please check the file name and try again.")
+            file_name = input(input_string)
+
+# Read stat file
+def read_stat_file(input_string):
+    file_found = False
+    global num_of_stats
+    file_name = input(input_string)
+
+    while not file_found:
+        try:
+            with open(file_name, "r") as file:
+                file_found = True # File found
+
+                num_of_stats = int(file.readline().strip()) # Initialize file number of stats with first line
+                
+                # Read file content and store it
+                for line in file:
+                    data = (line.split(':'))
+
+                    # Declare distionary for each event
+                    stat_dist = {
+                        "event_name": data[0].strip(),
+                        "mean": round(float(data[1].strip()), 2),
+                        "std_deviation": round(float(data[2].strip()), 2)
+                    }
+                    
+                    stats.append(stat_dist)
+                    # print(stat_dist)  # Debug line
+
+        except FileNotFoundError:
+            print("File not found. Please check the file name and try again.")
+            file_name = input(input_string)
+
+# Prompt user to input the file name
+read_event_file("Please enter the events file name: ")
+read_stat_file("Please enter the stats file name: ")
+
+#################
+# Debug Section #
+#################
+# Events
+print(f"Number of event: {num_of_events}")
+print("Stored events:")
+for event in events:
+    print(event)
+
+print()
+
+# Stats
+print(f"Number of stat: {num_of_stats}")
+print("Stored stats:")
+for stat in stats:
+    print(stat)
